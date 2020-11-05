63d4276ff19008747caf65d3bd115d18addab93b
Bernardo Costa Nascimento <bernardoc1104@gmail.com>
2020-11-05 16:50:16 -0300

    test(ng-material-theme): escrever testes para o schematics

--- src/ng-material-theme/index_spec.ts
+++ src/ng-material-theme/index_spec.ts
@@ -1,16 +1,78 @@
 import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import {
+  UnitTestTree,
+  SchematicTestRunner,
+} from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 
+const collectionPath = path.join(__dirname, "../collection.json");
+const runner = new SchematicTestRunner("schematics", collectionPath);
 
-const collectionPath = path.join(__dirname, '../collection.json');
+let appTree: UnitTestTree;
 
+describe("ng-material-theme", () => {
+  beforeEach(async () => {
+    appTree = await runner
+      .runExternalSchematicAsync(
+        "@schematics/angular",
+        "workspace",
+        { name: "test", version: "10.0.5" })
+      .toPromise();
 
-describe('ng-material-theme', () => {
-  it('works', async () => {
-    const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = await runner.runSchematicAsync('ng-material-theme', {}, Tree.empty()).toPromise();
+    appTree = await runner
+      .runExternalSchematicAsync(
+        "@schematics/angular",
+        "application",
+        { name: "my-app", style: 'scss', projectRoot: './' },
+        appTree
+      )
+      .toPromise();
+  });
 
-    expect(tree.files).toEqual([]);
+  it('should add the @angular/material schematic and copy the custom theme files', async () => {
+    const tree = await runner.runSchematicAsync(
+      'ng-material-theme',
+      {},
+      appTree
+    ).toPromise();
+
+    const packageFile = JSON.parse(tree.read('package.json')!.toString('utf-8'));
+
+    expect(packageFile.dependencies['@angular/material']).toBeTruthy();
+  });
+
+  it('should copy the custom theme files to the project root', async () => {
+    const tree = await runner.runSchematicAsync(
+      'ng-material-theme',
+      {},
+      appTree
+    ).toPromise();
+
+    expect(tree.files).toContain('/src/theme.scss');
+    expect(tree.files).toContain('/src/custom-component-themes.scss');
+  });
+
+  it("should update the \'styles.scss\' file on \'src\' folder", async () => {
+    const tree = await runner.runSchematicAsync(
+      'ng-material-theme',
+      {},
+      appTree
+    ).toPromise();
+
+    const styles = tree.read('/src/styles.scss')!.toString('utf-8');
+
+    expect(styles).toContain("@import '~@angular/material/theming';");
+    expect(styles).toContain("@import './custom-component-themes.scss';");
+    expect(styles).toContain("@import './theme.scss';");
+  });
+
+  it('should fail if missing tree', async () => {
+    await expectAsync(
+      runner.runSchematicAsync(
+        'ng-material-theme',
+        { name: 'invalid-workspace' },
+        Tree.empty(),
+      ).toPromise()
+    ).toBeRejected();
   });
 });
